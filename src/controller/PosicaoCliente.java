1cc26994a5041e26ea943a42f254e374351298e3
beatrizaurelio <42580170+beatrizaurelio@users.noreply.github.com>
2018-09-05 16:58:15 -0300

    Add files via upload

--- src/controller/PosicaoCliente.java
+++ src/controller/PosicaoCliente.java
@@ -2,38 +2,30 @@ package controller;
 
 import model.Posicao;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Logger;
-
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.*;
+import java.util.*;
 
 public class PosicaoCliente{
-
+	
     private Connection conn;
     private Statement statement;
     private ConexaoInterface conexao;
-    
-
-            
-    public void inserePosicaoCliente(Posicao posicao) throws ConexaoException{
+        
+  public void inserePosicaoCliente(Posicao posicao) throws ConexaoException, SQLException{
     	/*Param: ?
            Cria o Cliente no Banco (mySQL)
           retorna Objeto de Model com Posicao vazia*/
 	   
-    	//Connection con = conexao.getConnection();
-    	
-    	String query = "INSERT INTO posicaoCliente (classificacao, tipo fiscal, nomeativo, vencimento, taxa, valor) VALUES (?,?,?,?,?,?)";
-    	stmInserir = conexao.prepareStatement(query);
+    	String query = "INSERT INTO posicaoCliente (classificacaoPosicao, tipoFiscalPosicao, nomeAtivoPosicao, vencimentoPosicao, taxaPosicao, valorPosicao) VALUES (?,?,?,?,?,?)";
+    	statement = conexao.prepareStatement(query);
+        ResultSet rs = statement.executeQuery(query);
+        
     	
   }
     
-    public void buscaPosicaoCliente() throws ConexaoException{
+    public void buscaPosicaoCliente() throws ConexaoException, SQLException{
     	/* Param: access_token do Sugar (assumindo que o Login foi feito com sucesso)
         Verifica se o Cliente existe no Sugar para o usu�rio
          se existir procurar no banco (mySQL) a posi��o do Cliente
@@ -42,28 +34,40 @@ public class PosicaoCliente{
          se n�o existir enviar mensagem 
           "Cliente n�o existe / sem acesso ao Cliente"*/
 
-    	String query ="SELECT posicao FROM posicaoCliente WHERE codigoCliente=?";
-        stmObterPosicao = conexao.prepareStatement(query);
+    	String query ="SELECT * FROM posicaoCliente WHERE codigoCliente=?";
+        statement = conexao.prepareStatement(query);
+        ResultSet rs = statement.executeQuery(query);
+        
+        while(rs.next()){
+            String classificacaoPosicao = rs.getString("Classificacao");
+            String tipoFiscalPosicao = rs.getString("Tipo Fiscal");
+            String nomeAtivoPosicao = rs.getString("Nome Ativo");
+            float vencimentoPosicao = rs.getFloat("Vencimento");
+            float taxaPosicao = rs.getFloat("Taxa"); 
+            float valorPosicao = rs.getFloat("Valor");
+            
+        }
  
-    	
     }
 
 
-	public void atualizaPosicaoCliente() throws ConexaoException {
+	public void atualizaPosicaoCliente() throws ConexaoException, SQLException {
 		/*
-		 *  Param: objeto de Model com informacoees atualizadas de Poscao do Cliente
+		 *  Param: objeto de Model com informacoees atualizadas de Posicao do Cliente
                    Atualiza Cliente com base no objeto
 		 * */
     	
-		String query = "UPDATE posicaoCliente SET (classificacao, tipo fiscal, nomeativo, vencimento, taxa, valor) VALUES (?,?,?,?,?,?) WHERE codigoCliente=?";
-        stmAtualizar = conexao.prepareStatement(query);
-		
-	}
+            String query = "UPDATE posicaoCliente SET (classificacaoPosicao, tipoFiscalPosicao, nomeAtivoPosicao, vencimentoPosicao, taxaPosicao, valorPosicao) VALUES (?,?,?,?,?,?) WHERE codigoCliente=?";
+            statement = conexao.prepareStatement(query);
+            ResultSet rs = statement.executeQuery(query);	
 	
-	public void deletaPosicaoCliente() throws ConexaoException{
+        }
+	
+	public void deletaPosicaoCliente() throws ConexaoException, SQLException{
 	    			
-		String query = "DELETE FROM posicaoCliente WHERE codigoCliente=?";
-        stmApagar = conexao.prepareStatement(query);
+            String query = "DELETE FROM posicaoCliente WHERE codigoCliente=?";
+            statement = conexao.prepareStatement(query);
+            ResultSet rs = statement.executeQuery(query);
 	}
 			
 }
