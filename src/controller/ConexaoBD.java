d458aac22022f3a2f3b0c993a434c238756e9f1e
beatrizaurelio <42580170+beatrizaurelio@users.noreply.github.com>
2018-09-11 16:29:41 -0300

    Add files via upload

---
+++ src/controller/ConexaoBD.java
@@ -0,0 +1,63 @@
+package controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.*;
+import javax.swing.JOptionPane;
+
+public class ConexaoBD{
+
+    private String driver = "com.mysql.jdbc.Driver";
+    private String caminho = "jdbc:mysql://192.168.1.4:3306/alocacao";
+    private String usuario = "alocacao";
+    private String senha = "alocacao@2018";
+    
+    public ResultSet rs;
+    public Statement statement;
+    public Connection con;
+
+    
+    public ConexaoBD() {}
+
+    
+    public void open() throws ConexaoException {
+        System.setProperty("jdbc.Drivers", driver);
+        try {
+            con = DriverManager.getConnection(caminho, usuario, senha);
+            //JOptionPane.showMessageDialog(null, "Conexao efetuada com sucesso.");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao efetuar conexao." +ex.getMessage());
+        }
+    }
+
+    public void executaSql(String sql){
+        try {
+            statement = con.createStatement(rs.TYPE_SCROLL_INSENSITIVE, rs.CONCUR_READ_ONLY);
+            rs = statement.executeQuery(sql);
+        
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao executar sql./n" +ex.getMessage());
+        }    
+    } 
+
+    
+    public void close() throws ConexaoException {
+         try{
+            con.close();
+            
+        }catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "Erro ao fechar conexao.");
+        }
+    }
+
+    Statement prepareStatement(String query) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+	
+    
+}
--- src/controller/PosicaoCliente.java
+++ src/controller/PosicaoCliente.java
@@ -6,76 +6,81 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.*;
 import java.util.*;
+import javax.swing.JOptionPane;
 
 public class PosicaoCliente{
 	
-    private Connection conn;
-    private Statement statement;
-    private ConexaoInterface conexao;
+    private Connection con;
+    private Statement statement; 
     private PreparedStatement pst;
+    ConexaoBD conex = new ConexaoBD();
+    Posicao pos = new Posicao();
+    
+ 
         
-  public void inserePosicaoCliente(Posicao posicao) throws ConexaoException, SQLException{
+    public void inserePosicaoCliente(Posicao posicao) throws ConexaoException, SQLException{
     	/*Param: ?
            Cria o Cliente no Banco (mySQL)
           retorna Objeto de Model com Posicao vazia*/
 	   
-    	String query = "INSERT INTO posicaoCliente (classificacaoPosicao, tipoFiscalPosicao, nomeAtivoPosicao, vencimentoPosicao, taxaPosicao, valorPosicao) VALUES (?,?,?,?,?,?)";
-    	statement = conexao.prepareStatement(query);
-        ResultSet rs = statement.executeQuery(query);
+    	String query = "INSERT INTO alocacoes (classificacao, garantia_produto, ativo, data_vencimento, NET) VALUES (?,?,?,?,?)";
+    	pst = conex.con.prepareStatement(query);
         
+        pst.setString(1, pos.getClassificacao());
+        pst.setString(2, pos.getSubproduto());
+        pst.setString(3, pos.getAtivo());
+        pst.setDate(4, pos.getDataVencimento());
+        pst.setFloat(5, pos.getNet());
         
-        
-        
-        
-        
-        
-        
-    	
+        pst.execute();
+        	
   }
     
-    public void buscaPosicaoCliente() throws ConexaoException, SQLException{
+    public Posicao buscaPosicaoCliente(Posicao mod) throws ConexaoException, SQLException{
     	/* Param: access_token do Sugar (assumindo que o Login foi feito com sucesso)
-        Verifica se o Cliente existe no Sugar para o usu�rio
-         se existir procurar no banco (mySQL) a posi��o do Cliente
-         Armazenar em Objeto de Model ou retornar um objeto com informa��es
-         se cliente n�o tiver Posicao usa metodo CriaPosicaoCliente e retorna a Posicao Zerada
-         se n�o existir enviar mensagem 
-          "Cliente n�o existe / sem acesso ao Cliente"*/
-
-    	String query ="SELECT * FROM posicaoCliente WHERE codigoCliente=?";
-        statement = conexao.prepareStatement(query);
-        ResultSet rs = statement.executeQuery(query);
-        
-        while(rs.next()){
-            String classificacaoPosicao = rs.getString("Classificacao");
-            String tipoFiscalPosicao = rs.getString("Tipo Fiscal");
-            String nomeAtivoPosicao = rs.getString("Nome Ativo");
-            float vencimentoPosicao = rs.getFloat("Vencimento");
-            float taxaPosicao = rs.getFloat("Taxa"); 
-            float valorPosicao = rs.getFloat("Valor");
+         se cliente nao tiver Posicao usa metodo CriaPosicaoCliente e retorna a Posicao Zerada
+         */
+  
+        conex.open();
+        conex.executaSql("SELECT * FROM posicaoCliente WHERE id_cliente LIKE '%"+mod.getPesquisa()+"%'");
+        try {
+            conex.rs.first();
+            mod.setClassificacao(conex.rs.getString("classificacao"));
+            //PRODUTO GARANTIA = TIPO FISCAL ?
+            mod.setProdutoGarantia(conex.rs.getString("garantia_produto")); //??????????????
+            mod.setAtivo(conex.rs.getString("ativo"));
+            mod.setDataVencimento(conex.rs.getDate("data_vencimento"));
+            mod.setNet(conex.rs.getFloat("NET"));
             
+             
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Cliente não existe / Sem acesso ao cliente.");
         }
- 
+                
+        conex.close();
+        return mod;
     }
 
-
-	public void atualizaPosicaoCliente() throws ConexaoException, SQLException {
-		/*
-		 *  Param: objeto de Model com informacoees atualizadas de Posicao do Cliente
-                   Atualiza Cliente com base no objeto
-		 * */
-    	
-            String query = "UPDATE posicaoCliente SET (classificacaoPosicao, tipoFiscalPosicao, nomeAtivoPosicao, vencimentoPosicao, taxaPosicao, valorPosicao) VALUES (?,?,?,?,?,?) WHERE codigoCliente=?";
-            statement = conexao.prepareStatement(query);
-            ResultSet rs = statement.executeQuery(query);	
+    public void atualizaPosicaoCliente() throws ConexaoException, SQLException {
+   	
+        String query = "UPDATE alocacoes SET classificacao=?, garantia_produto=?, ativo=?, data_vencimento=?, NET=? WHERE id_cliente= ?";
+    	pst = conex.con.prepareStatement(query);
+        
+        pst.setString(1, pos.getClassificacao());
+        pst.setString(2, pos.getSubproduto());
+        pst.setString(3, pos.getAtivo());
+        pst.setDate(4, pos.getDataVencimento());
+        pst.setFloat(5, pos.getNet());
+        
+        pst.execute();	
 	
         }
 	
 	public void deletaPosicaoCliente() throws ConexaoException, SQLException{
 	    			
-            String query = "DELETE FROM posicaoCliente WHERE codigoCliente=?";
-            statement = conexao.prepareStatement(query);
-            ResultSet rs = statement.executeQuery(query);
+            String query = "DELETE FROM alocacoes WHERE id_cliente=?";
+            pst = conex.con.prepareStatement(query);
+            pst.execute(query);
 	}
        
 	
